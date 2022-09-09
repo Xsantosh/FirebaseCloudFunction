1d90d9fdd6b682344ceefa04e7bb94fd926aa77b
santosh <xsantosh7@gmail.com>
2022-09-09 17:30:45 +0530

    firebase auth events handle

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from "firebase-functions";
-import * as axios from 'axios';
-
+import axios = require('axios');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -9,11 +8,12 @@ import * as axios from 'axios';
 //   response.send("Hello from Firebase!");
 // });
 
-
+///Simple get method response from functions
 export const helloWorld = functions.https.onRequest((req,res)=>{
         res.send("Hello from functions");
 })
 
+///Request method management from Function call
 export const api = functions.https.onRequest((req,res) => {
     switch (req.method) {
         case 'GET':
@@ -35,9 +35,25 @@ export const api = functions.https.onRequest((req,res) => {
     }
 })
 
+///Third party api call from Functions
 export const serveRequest = functions.https.onRequest(async (req,res)=>{
     //@ts-ignore
     var data = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
     res.send(data.data);
     Promise.resolve();
 })
+
+///Firebase auth event handling
+
+///Firebase auth add user event handle
+export const onUserAdded = functions.auth.user().onCreate(user=> {
+    console.log(`user created ${user.email}`);
+    return Promise.resolve();
+});
+
+///Firebase auth delete user event handle
+export const onUserUpdate = functions.auth.user().onDelete(user =>{
+    console.log(`user deleted ${user.email}`);
+    return Promise.resolve();
+});
+
